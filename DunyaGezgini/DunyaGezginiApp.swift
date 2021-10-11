c44aec1fcf777535f73edfff8fd67845e2236b13
UmutGultekin <umut.gultekn@hotmail.com>
2021-10-11 22:05:50 +0300

    Initial Commit

---
+++ DunyaGezgini/DunyaGezginiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DunyaGezginiApp.swift
+//  DunyaGezgini
+//
+//  Created by Umut Gultekin on 11.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct DunyaGezginiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
